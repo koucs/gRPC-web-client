13a9d679251f6e901d4a01f93bbd1eda675c904d
koucs <kou.cshu@gmail.com>
2021-03-14 19:26:18 +0900

    add helloworld proto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {HelloRequest} from './pb-web/helloworld_pb';
+import {GreeterClient} from './pb-web/HelloworldServiceClientPb';
 
 function App() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onClick = async () => {
+    const request = new HelloRequest();
+    request.setName(name);
+    const client = new GreeterClient("http://172.24.245.102:8080");
+    const response = await client.sayHello(request, {});
+    setMessage(response.getMessage());
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +35,14 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <input 
+        type="text"
+        value={name}
+        onChange={onChange}
+      />
+      <button onClick={onClick}>Send</button>
+      <p>{message}</p>
     </div>
   );
 }
